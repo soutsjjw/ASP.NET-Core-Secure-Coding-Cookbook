f7cd3ba3863ca1f699a96d0b2e88c6999d43fcdc
souts jjw <souts.jjw@gmail.com>
2023-10-22 17:47:11 +0800

    使用 HtmlSanitizer 程式庫進行輸入清理

--- OnlineBankingApp/Models/FundTransfer.cs
+++ OnlineBankingApp/Models/FundTransfer.cs
@@ -8,37 +8,39 @@ namespace OnlineBankingApp.Models
     public class FundTransfer : IValidatableObject
     {
         public int ID { get; set; }
-        
+
         public int CustomerID { get; set; }
 
         [Display(Name = "Account From")]
         public int AccountFrom { get; set; }
 
         [Display(Name = "Account To")]
-        public int AccountTo { get; set; }        
-        
+        public int AccountTo { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime TransactionDate { get; set; }
 
-        [Column(TypeName = "decimal(18, 2)")]        
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Amount { get; set; }
 
         private string note;
 
         [StringLength(60)]
         [DataType(DataType.MultilineText)]
-        public string Note {
-            get => note;                      
-            set => note = value;                    
+        public string Note
+        {
+            get => note;
+            set => note = new HtmlSanitizer().Sanitize(value);
         }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (AccountFrom == AccountTo)
             {
-                yield return new ValidationResult("The fund transfer destination must be different from the source");
+                yield return new ValidationResult(
+                    "The fund transfer destination must be different from the source"
+                );
             }
         }
-
     }
 }
