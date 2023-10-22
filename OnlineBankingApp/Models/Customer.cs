078f6371419481748c17788177ac49cac5c0d637
souts jjw <souts.jjw@gmail.com>
2023-10-22 15:31:52 +0800

    使用驗證屬性啟用白名單驗證

--- OnlineBankingApp/Models/Customer.cs
+++ OnlineBankingApp/Models/Customer.cs
@@ -8,19 +8,22 @@ namespace OnlineBankingApp.Models
     {
         public int ID { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$", ErrorMessage = "First Name must contain only letters")]
         [Display(Name = "First Name")]
         [StringLength(60, MinimumLength = 3)]
-        [Required]        
+        [Required]
         public string FirstName { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$", ErrorMessage = "Middle Name must contain only letters")]
         [Display(Name = "Middle Name")]
         [StringLength(60, MinimumLength = 3)]
-        [Required]        
+        [Required]
         public string MiddleName { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$", ErrorMessage = "Last Name must contain only letters")]
         [Display(Name = "Last Name")]
         [StringLength(60, MinimumLength = 3)]
-        [Required]        
+        [Required]
         public string LastName { get; set; }
 
         [DataType(DataType.Date)]
